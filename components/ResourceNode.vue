de4b54a253c6c9220ea12cfebc74d2e5bd24f048
kunish <kunish.butt@gmail.com>
2024-03-24 13:04:55 +0800

    feat: subscriptions & nodes

--- components/ResourceNode.vue
+++ components/ResourceNode.vue
@@ -3,6 +3,8 @@ import * as queries from '~/queries'
 
 const apiStore = useAPIStore()
 
+const isNodeModalOpen = ref(false)
+
 const { data: nodes } = useAsyncData(async () =>
   (await apiStore.apiClient?.query(queries.nodes, {}))?.data?.nodes.edges.map(
     (node) => ({
@@ -14,5 +16,17 @@ const { data: nodes } = useAsyncData(async () =>
 </script>
 
 <template>
-  <UTable :rows="nodes" />
+  <div class="flex flex-col gap-4">
+    <div class="flex justify-end">
+      <UButton icon="i-heroicons-plus" @click="isNodeModalOpen = true" />
+    </div>
+
+    <UTable :rows="nodes" />
+
+    <UModal v-model="isNodeModalOpen">
+      <UCard>
+        <template #header>Import Nodes</template>
+      </UCard>
+    </UModal>
+  </div>
 </template>
--- components/ResourceSubscription.vue
+++ components/ResourceSubscription.vue
@@ -3,6 +3,8 @@ import * as queries from '~/queries'
 
 const apiStore = useAPIStore()
 
+const isSubscriptionModalOpen = ref(false)
+
 const { data: subscriptions } = useAsyncData(async () =>
   (
     await apiStore.apiClient?.query(queries.subscriptions, {})
@@ -14,5 +16,20 @@ const { data: subscriptions } = useAsyncData(async () =>
 </script>
 
 <template>
-  <UTable :rows="subscriptions" />
+  <div class="flex flex-col gap-4">
+    <div class="flex justify-end">
+      <UButton
+        icon="i-heroicons-link"
+        @click="isSubscriptionModalOpen = true"
+      />
+    </div>
+
+    <UTable :rows="subscriptions" />
+
+    <UModal v-model="isSubscriptionModalOpen">
+      <UCard>
+        <template #header>Import Subscriptions</template>
+      </UCard>
+    </UModal>
+  </div>
 </template>
